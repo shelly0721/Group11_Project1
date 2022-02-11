d791aa7a1b2a6085607bbd5a900421e751c296bc
sayaka <b082313w@gmail.com>
2022-02-11 16:17:16 -0500

    first commit

---
+++ src/softwaredevelopment_p1/Group11_Project1.java
@@ -0,0 +1,12 @@
+package softwaredevelopment_p1;
+/**
+ *
+ * @author Group_11
+ */
+public class Group11_Project1 {
+
+    public static void main(String[] args) {
+       
+    }
+    
+}
---
+++ src/softwaredevelopment_p1/GroupOfCards.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package softwaredevelopment_p1;
+
+/**
+ *
+ * @author Group_11
+ */
+public class GroupOfCards {
+    
+}
