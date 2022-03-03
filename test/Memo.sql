559ea36862e654e6335d93677a7ccf79a4687c96
sayaka <b082313w@gmail.com>
2022-03-02 21:11:08 -0500

    Add text file for memo

---
+++ test/Memo.sql
@@ -0,0 +1,10 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
+ */
+/**
+ * Author:  nakamurasayaka
+ * Created: Mar. 2, 2022
+ */
+
+We write memo and share the implementation of project.
