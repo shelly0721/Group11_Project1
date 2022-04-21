1b82323eab6d7b9a95b7f4b657c7a3f104eef66d
shelly0721 <98172794+shelly0721@users.noreply.github.com>
2022-04-21 16:23:56 -0400

    Update War.java

--- src/softwaredevelopment_p1/War.java
+++ src/softwaredevelopment_p1/War.java
@@ -11,12 +11,24 @@ public class War extends GroupOfCards {
     }
     
     
-    public void addToWinner(Player player) {
-
+   public int addToWinner (String player) { 
+        for (int i = getTemp().size(); i > 0; i-- ) {
+             // temp contains the cards taken from deck.
+             // move them to winner hands.
+             getCards().add(getTemp().get(0));
+             getTemp().remove(0);                       // in this way, temp should be declared in GroupOfCard         
+         }                                              // so far, it declared in "DiscardPile" class.
+         return getCards().size();      
+        
     }
     
-    public void showDeck() {
-    
+   public void showDeck() {
+        /* this method is called both when cards are put into deck and when 
+           cards are taken from the deck  */
+         for(Card e: getCards()) {
+            //Uses toString method in Card
+            System.out.println(e);
+        } 
     }
     
     
