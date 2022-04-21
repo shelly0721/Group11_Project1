5e7024e7f8b209d43668e4b1dc0a20b7c0547e53
shelly0721 <98172794+shelly0721@users.noreply.github.com>
2022-04-21 16:25:30 -0400

    Update DiscardPile.java

--- src/softwaredevelopment_p1/DiscardPile.java
+++ src/softwaredevelopment_p1/DiscardPile.java
@@ -1,25 +1,42 @@
 package softwaredevelopment_p1;
 
+import java.util.ArrayList;
 import java.util.Collections;
 
 public class DiscardPile extends GroupOfCards {
 
+    private ArrayList<Card> temp = new ArrayList<>();
+   
     public DiscardPile(int size) {
         super(size);
     }
            
-     public void addToWinner(Player player) {
-    
-
+    public void addToWinner (Player player) {   
+        for (int i = getCards().size()-1; i>0; i--) {
+            // move all cards in deck to temp, then remove it          
+            temp.add(getCards().get(0));
+            getCards().remove(0);  
+           
+        }
     }
     
     public void showDeck() {
-        System.out.println(getCards());
+        /* this method is called both when cards are put into deck and when 
+           cards are taken from the deck  */
+         for(Card e: getCards()){
+            //Uses toString method in Card
+            System.out.println(e);
+        }
     }
     
     
     public void shuffleCards() {
         Collections.shuffle(getCards());
     
+    }   
+    
+    public ArrayList<Card> getTemp() {
+            return temp;
     }
+    
 }
